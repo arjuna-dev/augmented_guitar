7d720403eb7aed71cb1badabb2f721246904c796
Arjuna-Dev <arjuna1@protonmail.com>
2022-10-06 14:25:40 +0200

    Working example

--- Arduino/Strumming_Detection_Hex_Pickup/Strumming_Detection_Hex_Pickup.ino
+++ Arduino/Strumming_Detection_Hex_Pickup/Strumming_Detection_Hex_Pickup.ino
@@ -1,125 +1,65 @@
 /*
-Hexaphonic Pickup Color Coding
-white: 6th string
-red: 4th string
+  Hexaphonic Pickup Color Coding
+  white: 6th string
+  red: 4th string
 */
-
 
 int string_values[6] = {0};
 int strings_last_values[6] = {0};
 int strings_last_note_on_values[6] = {0};
 int min_val = 10;
 int max_val = 800;
-int string_4_input = 22;
-int string_6_input = 21;
 int string_input_pins[2] = {21, 22};
+bool note_ons[6] = {0};
+bool plam_mutes[6] = {0};
+int hysteresis = 0;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(string_4_input, INPUT);
-  pinMode(string_6_input, INPUT);
+    Serial.begin(9600);
+    for (int i = 0; i > 6; i++) {
+      pinMode(string_input_pins[i], INPUT);
+    }
 }
 
-int string_4 = 0;
-int string_6 = 0;
-
-int string_4_last = 0;
-int string_4_last_note_on = 0;
-int hysteresis = 0;
-bool note_on = false;
-bool plam_mute = false;
-
-
-void note_detection(int string_value, int string_value_last, int string_number) {
-
-         //re-Pluck detection
-        if (string_value > strings_last_note_on_values[string_number]){
-            strings_last_note_on_values[string_number] = string_value;
-            note_on = true;
-            Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_value) + ", MIDI: " + map(string_value, 0, 700, 0, 127));
-            Serial.println("re-Pluck"); 
-        }
-        //Note_ON detection (value is higher than last)
-        else if (string_value > 50 && string_value > string_value_last && plam_mute==false){
-            strings_last_note_on_values[string_number] = string_value;
-            note_on = true;
-            Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_value) + ", MIDI: " + map(string_value, 0, 700, 0, 127));
-        }
-        //Palm Mute detection (value is lower than last)
-        else if ((strings_last_note_on_values[string_number]-string_value) > hysteresis && note_on == true){
-            note_on = false;
-            plam_mute = true;
-            Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_value) + ", MIDI: " + map(string_value, 0, 700, 0, 127));
-            Serial.println("PALM MUTE");
-        }
-        if (string_value < 20 && note_on == true){
-            note_on = false;
-            Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_value) + ", MIDI: " + map(string_value, 0, 700, 0, 127));
-            Serial.println("NOTE OFF");
-        }
-        if (string_value > string_value_last+hysteresis && plam_mute == true){
-            plam_mute = false;
-            Serial.println("UNMUTE");
-        }
-
+void note_detection(int string) {
+    //re-Pluck detection
+    if (string_values[string] > strings_last_note_on_values[string]) {
+      strings_last_note_on_values[string] = string_values[string];
+      note_ons[string] = true;
+      Serial.println("Note_ON: " + String(note_ons[string]) + " v:"  + String(string_values[string]) + ", MIDI: " + map(string_values[string], 0, 700, 0, 127));
+      Serial.println("re-Pluck");
+    }
+    //Note_ON detection (value is higher than last)
+    else if (string_values[string] > 50 && string_values[string] > strings_last_values[string] && plam_mutes[string] == false) {
+      strings_last_note_on_values[string] = string_values[string];
+      note_ons[string] = true;
+      Serial.println("Note_ON: " + String(note_ons[string]) + " v:"  + String(string_values[string]) + ", MIDI: " + map(string_values[string], 0, 700, 0, 127));
+    }
+    //Palm Mute detection (value is lower than last)
+    else if ((strings_last_note_on_values[string] - string_values[string]) > hysteresis && note_ons[string] == true) {
+      note_ons[string] = false;
+      plam_mutes[string] = true;
+      Serial.println("Note_ON: " + String(note_ons[string]) + " v:"  + String(string_values[string]) + ", MIDI: " + map(string_values[string], 0, 700, 0, 127));
+      Serial.println("PALM MUTE");
+    }
+    if (string_values[string] < 20 && note_ons[string] == true) {
+      note_ons[string] = false;
+      Serial.println("Note_ON: " + String(note_ons[string]) + " v:"  + String(string_values[string]) + ", MIDI: " + map(string_values[string], 0, 700, 0, 127));
+      Serial.println("NOTE OFF");
+    }
+    if (string_values[string] > strings_last_values[string] + hysteresis && plam_mutes[string] == true) {
+      plam_mutes[string] = false;
+      Serial.println("UNMUTE");
+    }
+  
+    strings_last_values[string] = string_values[string];
 }
 
 void loop() {
-        for (int i=0; i<2; i++) {
-          string_values[i] = analogRead(string_input_pins[i]);
-        }
-//            string_4 = analogRead(string_4_input);
-//            Serial.print("string_4: ");
-//            Serial.print(string_4);
-//            Serial.print(" ");
-//            string_6 = analogRead(string_6_input);
-//            Serial.print("string_6: ");
-//            Serial.println(string_6);
-//            Serial.print(" ");
-            
-        delay(1);
-        hysteresis = string_4*0.2;
-        
-        for (int i=0; i<2; i++) {
-          note_detection(string_values[i], strings_last_values[i], i);
-        }
-
-        for (int i=0; i<2; i++) {
-          strings_last_values[i] = string_values[i];
-        }
+  for (int i = 0; i < 2; i++) {
+    string_values[i] = analogRead(string_input_pins[i]);
+    delay(1);
+    hysteresis = string_values[i] * 0.2;
+    note_detection(i);
+  }
 }
-
-
-
-//             //re-Pluck detection
-//            if (string_4 > string_4_last_note_on){
-//                string_4_last_note_on = string_4;
-//                note_on = true;
-//                Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_4) + ", MIDI: " + map(string_4, 0, 700, 0, 127));
-//                Serial.println("re-Pluck"); 
-//            }
-//            //Note_ON detection (value is higher than last)
-//            else if (string_4 > 50 && string_4 > string_4_last && plam_mute==false){
-//                string_4_last_note_on = string_4;
-//                note_on = true;
-//                Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_4) + ", MIDI: " + map(string_4, 0, 700, 0, 127));
-//            }
-//            //Palm Mute detection (value is lower than last)
-//            else if ((string_4_last_note_on-string_4) > hysteresis && note_on == true){
-//                note_on = false;
-//                plam_mute = true;
-//                Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_4) + ", MIDI: " + map(string_4, 0, 700, 0, 127));
-//                Serial.println("PALM MUTE");
-//            }
-//            if (string_4 < 20 && note_on == true){
-//                note_on = false;
-//                Serial.println("Note_ON: " + String(note_on) + " v:"  + String(string_4) + ", MIDI: " + map(string_4, 0, 700, 0, 127));
-//                Serial.println("NOTE OFF");
-//            }
-//            if (string_4 > string_4_last+hysteresis && plam_mute == true){
-//                plam_mute = false;
-//                Serial.println("UNMUTE");
-//            }
-//            
-//            string_4_last = string_4;
-//}
