3078c0362ec903237ce405d042512fc77e6707ca
Arjuna-Dev <arjuna1@protonmail.com>
2023-02-12 16:57:21 -0800

    Added MIDI support to web app

--- Vue_CLI_App/luxguitar/src/App.vue
+++ Vue_CLI_App/luxguitar/src/App.vue
@@ -7,6 +7,41 @@
 <script>
 import Screen from "./components/Screen.vue";
 
+let midi = null; // global MIDIAccess object
+function onMIDISuccess(midiAccess) {
+  console.log("MIDI ready!");
+  midi = midiAccess; // store in the global (in real usage, would probably keep in an object instance)
+  listInputsAndOutputs(midi);
+}
+
+function onMIDIFailure(msg) {
+  console.error(`Failed to get MIDI access - ${msg}`);
+}
+
+navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+function listInputsAndOutputs(midiAccess) {
+  for (const entry of midiAccess.inputs) {
+    const input = entry[1];
+    console.log(
+      `Input port [type:'${input.type}']` +
+        ` id:'${input.id}'` +
+        ` manufacturer:'${input.manufacturer}'` +
+        ` name:'${input.name}'` +
+        ` version:'${input.version}'`
+    );
+  }
+
+  for (const entry of midiAccess.outputs) {
+    const output = entry[1];
+    console.log(
+      `Output port [type:'${output.type}'] id:'${output.id}' manufacturer:'${output.manufacturer}' name:'${output.name}' version:'${output.version}'`
+    );
+  }
+}
+
+
+
 export default {
   name: "app",
   components: {
