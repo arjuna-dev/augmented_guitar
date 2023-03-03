5b3c5bec999f153b1a0525979b514645191e9151
Arjuna-Dev <arjuna1@protonmail.com>
2023-03-03 16:34:22 +0100

    Missing type error fix

--- Arduino/Augmented_Guitar/Augmented_Guitar.ino
+++ Arduino/Augmented_Guitar/Augmented_Guitar.ino
@@ -120,17 +120,17 @@ void setup(){
   }
 }
 
-void MIDI_press_fret(note, fret){
+void MIDI_press_fret(int note, int fret){
   usbMIDI.sendControlChange(20, fret, 1);
   usbMIDI.sendNoteOn(note, 0, 1);
 }
 
-void MIDI_note_on(note, velocity, fret){
+void MIDI_note_on(int note, int velocity, int fret){
   usbMIDI.sendControlChange(20, fret, 1);
   usbMIDI.sendNoteOn(note, velocity, 1);
 }
 
-void MIDI_note_off(note, velocity, fret){
+void MIDI_note_off(int note, int velocity, int fret){
   usbMIDI.sendControlChange(20, fret, 1);
   usbMIDI.sendNoteOff(note, velocity, 1);
 }
