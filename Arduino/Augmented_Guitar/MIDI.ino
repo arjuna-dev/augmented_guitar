99f8e8748f6d71ed609358d42a0d7deddf0a9068
Ale Camus <ale.camus@native-instruments.de>
2023-03-12 15:13:54 +0100

    Gave specific cc values to press and lift for clarity

--- Arduino/Augmented_Guitar/MIDI.ino
+++ Arduino/Augmented_Guitar/MIDI.ino
@@ -1,10 +1,10 @@
 void MIDI_press_fret(int note, int fret) {
-  usbMIDI.sendControlChange(20, fret, 1);
+  usbMIDI.sendControlChange(21, fret, 1);
   usbMIDI.sendNoteOn(note, 0, 1);
 }
 
 void MIDI_lift_fret(int note, int fret) {
-  usbMIDI.sendControlChange(21, fret, 1);
+  usbMIDI.sendControlChange(22, fret, 1);
   usbMIDI.sendNoteOn(note, 0, 1);
 }
 
