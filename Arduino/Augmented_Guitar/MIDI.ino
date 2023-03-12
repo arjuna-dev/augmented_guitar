0105fbc97d26c90203909218763f64cf17b1c5b2
Ale Camus <ale.camus@native-instruments.de>
2023-03-12 17:56:47 +0100

    Added cc cases

--- Arduino/Augmented_Guitar/MIDI.ino
+++ Arduino/Augmented_Guitar/MIDI.ino
@@ -1,10 +1,10 @@
 void MIDI_press_fret(int note, int fret) {
-  usbMIDI.sendControlChange(21, fret, 1);
+  usbMIDI.sendControlChange(22, fret, 1);
   usbMIDI.sendNoteOn(note, 0, 1);
 }
 
 void MIDI_lift_fret(int note, int fret) {
-  usbMIDI.sendControlChange(22, fret, 1);
+  usbMIDI.sendControlChange(23, fret, 1);
   usbMIDI.sendNoteOn(note, 0, 1);
 }
 
@@ -14,6 +14,6 @@ void MIDI_note_on(int note, int velocity, int fret) {
 }
 
 void MIDI_note_off(int note, int fret) {
-  usbMIDI.sendControlChange(20, fret, 1);
+  usbMIDI.sendControlChange(21, fret, 1);
   usbMIDI.sendNoteOff(note, 0, 1);
 }
