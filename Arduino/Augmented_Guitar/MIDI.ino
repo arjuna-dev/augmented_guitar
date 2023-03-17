93c16f6f7d954fa7159987a39e2ca33bce4f49c0
Ale Camus <ale.camus@native-instruments.de>
2023-03-17 10:30:52 +0100

    MIDI messages are now based on string and fret instead of MIDI and fret. Less error prone

--- Arduino/Augmented_Guitar/MIDI.ino
+++ Arduino/Augmented_Guitar/MIDI.ino
@@ -1,19 +1,23 @@
-void MIDI_press_fret(int note, int fret) {
+void MIDI_press_fret(int string, int fret) {
+  int note = MIDI_open_string_notes[string] + fret;
   usbMIDI.sendControlChange(22, fret, 1);
   usbMIDI.sendNoteOn(note, 0, 1);
 }
 
-void MIDI_lift_fret(int note, int fret) {
+void MIDI_lift_fret(int string, int fret) {
+  int note = MIDI_open_string_notes[string] + fret;
   usbMIDI.sendControlChange(23, fret, 1);
   usbMIDI.sendNoteOn(note, 0, 1);
 }
 
-void MIDI_note_on(int note, int velocity, int fret) {
+void MIDI_note_on(int string, int velocity, int fret) {
+  int note = MIDI_open_string_notes[string] + fret;
   usbMIDI.sendControlChange(20, fret, 1);
   usbMIDI.sendNoteOn(note, velocity, 1);
 }
 
-void MIDI_note_off(int note, int fret) {
+void MIDI_note_off(int string, int fret) {
+  int note = MIDI_open_string_notes[string] + fret;
   usbMIDI.sendControlChange(21, fret, 1);
   usbMIDI.sendNoteOff(note, 0, 1);
 }
