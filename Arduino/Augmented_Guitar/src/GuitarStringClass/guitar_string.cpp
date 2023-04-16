e6bb2b3e12e61277a2a10aea5fe20996df7d07ba
Ale Camus <ale.camus@native-instruments.de>
2023-04-16 13:06:15 +0200

    Moved logic to class

--- Arduino/Augmented_Guitar/src/GuitarStringClass/guitar_string.cpp
+++ Arduino/Augmented_Guitar/src/GuitarStringClass/guitar_string.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include "guitar_string.h"
 #include "../MIDI/midi.h"
+#include "../debug/debug.h"
 
 #define sine_wave_falling_edge 35
 #define peak_diff_threshold 30
@@ -31,52 +32,25 @@ void GuitarString::updateStringMIDIValue() {
       is_fret_touched = false;
     }
     if (is_last_fret_touched && is_fret_touched && !_note_on) {
-      pressed_fret = j + 1;
+      _pressed_fret = j + 1;
     }
     is_last_fret_touched = is_fret_touched;
   }
 
   if (!any_fret_touched) {
-    pressed_fret = 0;
+    _pressed_fret = 0;
   }
 
-  if (pressed_fret != _last_sent_pressed_fret) {
+  if (_pressed_fret != _last_sent_pressed_fret) {
     MIDI_lift_fret(_string_number, _last_sent_pressed_fret);
-    MIDI_press_fret(_string_number, pressed_fret);
-    _last_sent_pressed_fret = pressed_fret;
+    MIDI_press_fret(_string_number, _pressed_fret);
+    _last_sent_pressed_fret = _pressed_fret;
   }
 
 }
 
-
-void GuitarString::update_peak_value() {
-  if (_current_amplitude > _min_threshold) {
-    if (_current_amplitude < _previous_amplitude - sine_wave_falling_edge) {
-      _peak_value = _previous_amplitude;
-      _peak_value = map(_peak_value, 0, _max_amplitude, 0, 127);
-      if (_peak_value > 127) {
-        _peak_value = 127;
-      }
-    } else {
-      _peak_value = 0;
-    }
-  } else {
-    _peak_value = 0;
-  }
-  _previous_amplitude = _current_amplitude;
-}
-
-void GuitarString::get_current_amplitude() {
+void GuitarString::update_current_amplitude() {
   _current_amplitude = analogRead(_input_pin);
-}
-
-void GuitarString::detect_note_on() {
-  if (_peak_value > _last_peak_value + peak_diff_threshold && !_note_on) {
-    _note_on = true;
-    _note_on_timestamp = millis();
-    MIDI_note_on(_string_number, _peak_value, pressed_fret);
-    _last_sent_note_on_fret = pressed_fret;
-  }
 }
 
 void GuitarString::detect_note_off() {
@@ -93,8 +67,85 @@ void GuitarString::detect_note_off() {
   }
 }
 
+void GuitarString::update_peak_value() {
+  if (_current_amplitude > _min_threshold) {
+    if (_current_amplitude < _previous_amplitude - sine_wave_falling_edge) {
+      _peak_value = _previous_amplitude;
+      _peak_value = map(_peak_value, 0, _max_amplitude, 0, 127);
+      if (_peak_value > 127) {
+        _peak_value = 127;
+      }
+    } else {
+      _peak_value = 0;
+    }
+  } else {
+    _peak_value = 0;
+  }
+}
+
+void GuitarString::detect_note_on() {
+  if (_peak_value > _last_peak_value + peak_diff_threshold && !_note_on) {
+    _note_on = true;
+    _note_on_timestamp = millis();
+    MIDI_note_on(_string_number, _peak_value, _pressed_fret);
+    _last_sent_note_on_fret = _pressed_fret;
+  }
+}
+
 void GuitarString::update_last_peak_value() {
   if (_peak_value) {
       _last_peak_value = _peak_value;
+  }
+}
+
+void GuitarString::update_previous_amplitude() {
+  _previous_amplitude = _current_amplitude;
+}
+
+
+int GuitarString::get_MIDI_value() {
+  return MIDI_open_string_notes[_pressed_fret];
+}
+
+void GuitarString::detect_note_on_off() {
+  update_current_amplitude();
+  detect_note_off();
+  update_peak_value();
+  detect_note_on();
+  update_last_peak_value();
+  update_previous_amplitude();
+}
+
+int GuitarString::get_current_amplitude(){
+  return _current_amplitude;
+}
+
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+// _-_-_-_-_-_-_-_-_-_-_- Debug _-_-_-_-_-_-_-_-_-_-_-_-_-
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+#define NUM_OF_ARR_VALUES 1000
+#define hysteresis 25
+bool sine_wave_started = false;
+int record_sine_wave_counter = 0;
+int sine_wave_array[NUM_OF_ARR_VALUES];
+
+void GuitarString::printSineWaveValues(int iteration, int number_of_iterations) {
+  if (_string_number == iteration) {
+
+    if (sine_wave_started == false && _current_amplitude > _min_threshold && _current_amplitude > _previous_amplitude + hysteresis) {
+      Serial.println("in");
+      sine_wave_started = true;
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    if (sine_wave_started && record_sine_wave_counter < number_of_iterations) {
+      sine_wave_array[record_sine_wave_counter] = _current_amplitude;
+      record_sine_wave_counter++;
+    }
+    if (record_sine_wave_counter == number_of_iterations - 1) {
+      for (int j = 0; j < number_of_iterations - 1; j++) {
+        Serial.println(sine_wave_array[j]);
+      }
+    }
   }
 }
