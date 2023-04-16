89bf964ea4cecc61ab6447d2a2af14f91355b47b
Ale Camus <ale.camus@native-instruments.de>
2023-04-16 12:59:38 +0200

    Rearranged member variables

--- Arduino/Augmented_Guitar/src/GuitarStringClass/guitar_string.h
+++ Arduino/Augmented_Guitar/src/GuitarStringClass/guitar_string.h
@@ -8,15 +8,23 @@ class GuitarString {
 
 public:
   GuitarString(const int string_number = 0, const int input_pin = 0, const char open_string_note = 0, const int max_amplitude = 0, const int min_threshold = 0, const int max_wave_period = 0, int* touch_analog_values = 0, int* touch_reference_analog_values = 0);
+  void detect_note_on_off();
   void updateStringMIDIValue();
-  void update_peak_value();
-  void detect_note_on();
-  void detect_note_off();
-  void get_current_amplitude();
-  void update_last_peak_value();
-  int pressed_fret = 0;
+
+  int get_current_amplitude();
+  int get_MIDI_value();
+  
+  void printSineWaveValues(int iteration, int number_of_iterations);
+  void update_previous_amplitude();
 
 private:
+  void update_current_amplitude();
+  void detect_note_off();
+  void update_peak_value();
+  void detect_note_on();
+  void update_last_peak_value();
+
+  int _pressed_fret = 0;
   int _string_number;
   int _input_pin;
   int _open_string_note;
@@ -26,7 +34,6 @@ private:
   int _capacitance_threshold = 5000;
   int* _touch_analog_values;
   int* _touch_reference_analog_values;
-
   bool _note_on = false;
   unsigned long _note_on_timestamp = 0;
   int _current_amplitude = 0;
