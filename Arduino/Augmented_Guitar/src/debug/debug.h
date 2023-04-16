4dae8d59157959bf18df8220e41bd5b389a17297
Ale Camus <ale.camus@native-instruments.de>
2023-04-16 13:05:45 +0200

    Deleted duplicated dclarations

--- Arduino/Augmented_Guitar/src/debug/debug.h
+++ Arduino/Augmented_Guitar/src/debug/debug.h
@@ -1,17 +1,10 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+
+void printCurrentMIDIValues();
 void printSineWaveValues(int string_number, int iteration, int number_of_iterations);
-void printMIDIValues();
 
-constexpr int NUM_OF_ARR_VALUES = 1000;
-
-extern bool sine_wave_started;
-extern int record_sine_wave_counter;
-extern int sine_wave_array[NUM_OF_ARR_VALUES];
-
-void printTouchedMIDIValues();
-void printSineWaveValues(int string_number, int iteration, int number_of_iterations);
 void println(String str1);
 void println(int var);
 void println(String str1, int var1);
