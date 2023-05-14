4e3701ddd6d9e422c83ca5e59c537b5bb1a67d61
Ale Camus <ale.camus@native-instruments.de>
2023-05-14 13:04:23 +0200

    Deleted unused

--- Arduino/Augmented_Guitar/src/debug/debug.h
+++ Arduino/Augmented_Guitar/src/debug/debug.h
@@ -4,8 +4,6 @@
 #include "../GuitarStringClass/guitar_string.h"
 
 void printCurrentMIDIValues(GuitarString guitar_strings[6]);
-void printSineWaveValues(int string_number, int iteration, int number_of_iterations);
-
 void println(String str1);
 void println(int var);
 void println(String str1, int var1);
