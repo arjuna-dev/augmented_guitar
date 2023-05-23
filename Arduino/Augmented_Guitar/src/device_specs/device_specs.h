56cd494846973c32c517301885e98473a9178587
Ale Camus <ale.camus@native-instruments.de>
2023-05-23 13:16:58 +0200

    Made constexpr, improved performance

--- Arduino/Augmented_Guitar/src/device_specs/device_specs.h
+++ Arduino/Augmented_Guitar/src/device_specs/device_specs.h
@@ -9,7 +9,7 @@ constexpr int NUM_OF_FRETS = 4;
 constexpr int NUM_OF_NOTES = 32;
 constexpr int NUM_OF_MUX_PINS = 2;
 
-const int fret_position_to_mux[NUM_OF_STRINGS][NUM_OF_FRETS][2] = {
+constexpr int fret_position_to_mux[NUM_OF_STRINGS][NUM_OF_FRETS][2] = {
   {{mux_sig_pin_2, 15}, {mux_sig_pin_2, 9}, {mux_sig_pin_2, 3}, {mux_sig_pin_1, 2}},
   {{mux_sig_pin_2, 14}, {mux_sig_pin_2, 8}, {mux_sig_pin_2, 2}, {mux_sig_pin_1, 3}},
   {{mux_sig_pin_2, 13}, {mux_sig_pin_2, 7}, {mux_sig_pin_2, 1}, {mux_sig_pin_1, 4}},
